a06100571a4ff7f002d877a0a4e29d9ea19c157b
JanMalch <25508038+JanMalch@users.noreply.github.com>
2020-09-22 17:01:13 +0200

    chore: rename to angular-form-states-demo

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('angular-material-error-state-matching app is running!');
+    expect(page.getTitleText()).toEqual('aangular-form-states-demo app is running!');
   });
 
   afterEach(async () => {
